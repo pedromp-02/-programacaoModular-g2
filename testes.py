91f845bfb8e5871ae9a48581eca0d630becece76
Isomo1 <86137378+Isomo1@users.noreply.github.com>
2022-05-10 20:42:10 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,15 +1,15 @@
 from casingComp import *
 
-def comparacao_t1():
-    assert test_compara('fine', 'fine') == True
-    assert test_compara('fine', 'FINE') == True
-    assert test_compara('fine', 'Fine') == True
-    assert test_compara('fine', 'fINE') == True
-    assert test_compara('fine', 'fiNE') == True
-    assert test_compara('fine', 'finE') == True
-    assert test_compara('fine', 'fInE') == True
-    assert test_compara('fine', 'fINe') == True
-    assert test_compara('fine', 'FInE') == True
-    assert test_compara('fine', 'FinE') == True    
-    assert test_compara('fine', 'FiNE') == True
-    assert test_compara('fine', 'FINe') == True
+def teste_comparacao():
+    assert compara('fine', 'fine') == True
+    assert compara('fine', 'FINE') == True
+    assert compara('fine', 'Fine') == True
+    assert compara('fine', 'fINE') == True
+    assert compara('fine', 'fiNE') == True
+    assert compara('fine', 'finE') == True
+    assert compara('fine', 'fInE') == True
+    assert compara('fine', 'fINe') == True
+    assert compara('fine', 'FInE') == True
+    assert compara('fine', 'FinE') == True    
+    assert compara('fine', 'FiNE') == True
+    assert compara('fine', 'FINe') == True
