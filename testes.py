c39a164c46682332b84e57749906ae737d095a20
Isomo1 <86137378+Isomo1@users.noreply.github.com>
2022-05-10 20:29:12 -0300

    primeira implementação

--- testes.py
+++ testes.py
@@ -1,1 +1,15 @@
+from casingComp import *
 
+def comparacao_t1():
+    assert compara('fine', 'fine') == True
+    assert compara('fine', 'FINE') == True
+    assert compara('fine', 'Fine') == True
+    assert compara('fine', 'fINE') == True
+    assert compara('fine', 'fiNE') == True
+    assert compara('fine', 'finE') == True
+    assert compara('fine', 'fInE') == True
+    assert compara('fine', 'fINe') == True
+    assert compara('fine', 'FInE') == True
+    assert compara('fine', 'FinE') == True    
+    assert compara('fine', 'FiNE') == True
+    assert compara('fine', 'FINe') == True
