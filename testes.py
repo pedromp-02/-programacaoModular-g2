6b44ee02177c64e30f9ba7aa8b50dfb7fb62ecc2
Isomo1 <86137378+Isomo1@users.noreply.github.com>
2022-05-10 20:44:40 -0300

    sabotando teste

--- testes.py
+++ testes.py
@@ -2,6 +2,7 @@ from casingComp import *
 
 def test_comparacao():
     assert compara('fine', 'fine') == True
+    assert compara('fine', 'fine') == True
     assert compara('fine', 'FINE') == True
     assert compara('fine', 'Fine') == True
     assert compara('fine', 'fINE') == True
