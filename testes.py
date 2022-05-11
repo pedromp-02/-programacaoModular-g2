0860db6adda1cb22669bcafc68d6e7d87c276c97
Igor <86137378+Isomo1@users.noreply.github.com>
2022-05-10 21:33:01 -0300

    novos testes adicionados

--- testes.py
+++ testes.py
@@ -21,6 +21,26 @@ def test_comparacao():
     assert compara('fINE', 'FINE') == True
     assert compara('fINE', 'fINE') == True
     
+    #assert compara('fine', 'nice') == False
+    #assert compara('fine', 'nICE') == False
+    #assert compara('fine', 'NICE') == False
+    #assert compara('fine', 'Nice') == False
+    
+    #assert compara('Fine', 'nice') == False
+    #assert compara('Fine', 'nICE') == False
+    #assert compara('Fine', 'NICE') == False
+    #assert compara('Fine', 'Nice') == False
+    
+    #assert compara('fINE', 'nice') == False
+    #assert compara('fINE', 'nICE') == False
+    #assert compara('fINE', 'NICE') == False
+    #assert compara('fINE', 'Nice') == False
+    
+    #assert compara('FINE', 'nice') == False
+    #assert compara('FINE', 'nICE') == False
+    #assert compara('FINE', 'NICE') == False
+    #assert compara('FINE', 'Nice') == False
+    
 def test_casing():
     assert casing('FINE', 'good') == 'GOOD'
     #assert casing('Fine', 'good') == 'Good'
