1e6495012a001f88420aa4cc39805d11d4cb52b3
Isomo1 <86137378+Isomo1@users.noreply.github.com>
2022-05-10 21:09:15 -0300

    refatoração + teste da função casing

--- testes.py
+++ testes.py
@@ -2,27 +2,28 @@ from casingComp import *
 
 def test_comparacao():
     assert compara('fine', 'fine') == True
-    assert compara('fine', 'fINE') == True
-    assert compara('fine', 'fiNE') == True
-    assert compara('fine', 'finE') == True
-    assert compara('fine', 'fInE') == True
-    assert compara('fine', 'fINe') == True
-    assert compara('fine', 'FINE') == True
     assert compara('fine', 'Fine') == True
-    assert compara('fine', 'FInE') == True
-    assert compara('fine', 'FinE') == True    
-    assert compara('fine', 'FiNE') == True
-    assert compara('fine', 'FINe') == True
+    assert compara('fine', 'FINE') == True
+    assert compara('fine', 'fINE') == True
     
     assert compara('Fine', 'fine') == True
-    assert compara('Fine', 'fINE') == True
-    assert compara('Fine', 'fiNE') == True
-    assert compara('Fine', 'finE') == True
-    assert compara('Fine', 'fInE') == True
-    assert compara('Fine', 'fINe') == True
-    assert compara('Fine', 'FINE') == True
     assert compara('Fine', 'Fine') == True
-    assert compara('Fine', 'FInE') == True
-    assert compara('Fine', 'FinE') == True    
-    assert compara('Fine', 'FiNE') == True
-    assert compara('Fine', 'FINe') == True
+    assert compara('Fine', 'FINE') == True
+    assert compara('Fine', 'fINE') == True
+    
+    assert compara('FINE', 'fine') == True
+    assert compara('FINE', 'Fine') == True
+    assert compara('FINE', 'FINE') == True
+    assert compara('FINE', 'fINE') == True
+    
+    assert compara('fINE', 'fine') == True
+    assert compara('fINE', 'Fine') == True
+    assert compara('fINE', 'FINE') == True
+    assert compara('fINE', 'fINE') == True
+    
+ def test_casing():
+    assert casing('FINE', 'good') == 'GOOD'
+    #assert casing('Fine', 'good') == 'Good'
+    #assert casing('fINE', 'good') == 'gOOD'
+    #assert casing('fine', 'good') == 'good'
+  
