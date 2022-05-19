2f18dd7df1d65282a204ee28ab623ebe79e3522b
Igor <86137378+Isomo1@users.noreply.github.com>
2022-05-19 16:40:55 -0300

    Refatoração + novos testes

--- testes.py
+++ testes.py
@@ -1,9 +1,7 @@
 from casingComp import *
 from manipulacao import *
 
-# não precisa comentar os testes com erro, é bom para desenvolver se baseando em que testes faltam passar
-
-def test_compara_palavrascao():
+def test_compara_palavras():
     assert compara_palavras('fine', 'fine') == True
     assert compara_palavras('fine', 'Fine') == True
     assert compara_palavras('fine', 'FINE') == True
@@ -45,6 +43,9 @@ def test_compara_palavrascao():
     assert compara_palavras('FINE', 'Nice') == False
     
 def test_match_casing():
+    assert match_casing('!*(&', 'test') == 'test'
+    assert match_casing('221', 'test') == 'test'
+        
     assert match_casing('I', 'i') == 'I'
     assert match_casing('i', 'i') == 'i' 
     
@@ -54,51 +55,73 @@ def test_match_casing():
     assert match_casing('HE', 'my') == 'MY'
     assert match_casing('He', 'my') == 'My' 
     assert match_casing('he', 'my') == 'my'
+    assert match_casing('hE', 'my') == 'my' 
     
     assert match_casing('HI', 'hello') == 'HELLO'
     assert match_casing('Hi', 'hello') == 'Hello' 
     assert match_casing('hi', 'hello') == 'hello'
+    assert match_casing('hI', 'hello') == 'hello'    
     
     assert match_casing('TRY', 'dry') == 'DRY'
     assert match_casing('Try', 'dry') == 'Dry'
     assert match_casing('try', 'dry') == 'dry'
+    assert match_casing('tRY', 'dry') == 'dry'    
+    assert match_casing('trY', 'dry') == 'dry'
+    assert match_casing('TrY', 'dry') == 'dry'
+    assert match_casing('TRy', 'dry') == 'dry'
     
     assert match_casing('WHY', 'where') == 'WHERE'
     assert match_casing('Why', 'where') == 'Where'
     assert match_casing('why', 'where') == 'where'
+    assert match_casing('wHY', 'where') == 'where'    
+    assert match_casing('whY', 'where') == 'where'
+    assert match_casing('WhY', 'where') == 'where'
+    assert match_casing('WHy', 'where') == 'where'
     
     assert match_casing('FINE', 'good') == 'GOOD'
     assert match_casing('Fine', 'good') == 'Good'
     assert match_casing('fine', 'good') == 'good'
-   
-    assert match_casing('SOUP', 'vegetable') == 'VEGETABLE'
-    assert match_casing('Soup', 'vegetable') == 'Vegetable'
-    assert match_casing('soup', 'vegetable') == 'vegetable'
-    
-    assert match_casing('LIGHT', 'heavy') == 'HEAVY'
-    assert match_casing('Light', 'heavy') == 'Heavy'
-    assert match_casing('light', 'heavy') == 'heavy'
-    
-    assert match_casing('RANCH', 'hyperbole') == 'HYPERBOLE'
-    assert match_casing('Ranch', 'hyperbole') == 'Hyperbole'
-    assert match_casing('ranch', 'hyperbole') == 'hyperbole'
-    
-    assert match_casing('HEIGHT', 'crouch') == 'CROUCH'
-    assert match_casing('Height', 'crouch') == 'Crouch'
-    assert match_casing('height', 'crouch') == 'crouch'
-    
-    assert match_casing('MIGHTY', 'henceforth') == 'HENCEFORTH'
-    assert match_casing('Mighty', 'henceforth') == 'Henceforth'
-    assert match_casing('mighty', 'henceforth') == 'henceforth'
-        
     assert match_casing('fINE', 'good') == 'good'    
     assert match_casing('FinE', 'good') == 'good'
     assert match_casing('fiNE', 'good') == 'good'
     assert match_casing('fIne', 'good') == 'good'
-    assert match_casing('fINe', 'good') == 'good'
-    assert match_casing('FIne', 'good') == 'good'
-    assert match_casing('FiNe', 'good') == 'good'
-    assert match_casing('FInE', 'good') == 'good'
-    assert match_casing('FINe', 'good') == 'good'
-  
+   
+    assert match_casing('SOUP', 'vegetable') == 'VEGETABLE'
+    assert match_casing('Soup', 'vegetable') == 'Vegetable'
+    assert match_casing('soup', 'vegetable') == 'vegetable'
+    assert match_casing('sOUP', 'vegetable') == 'vegetable'    
+    assert match_casing('SouP', 'vegetable') == 'vegetable'
+    assert match_casing('soUP', 'vegetable') == 'vegetable'
+    assert match_casing('sOup', 'vegetable') == 'vegetable'
     
+    assert match_casing('LIGHT', 'heavy') == 'HEAVY'
+    assert match_casing('Light', 'heavy') == 'Heavy'
+    assert match_casing('light', 'heavy') == 'heavy'
+    assert match_casing('lIGHT', 'heavy') == 'heavy'    
+    assert match_casing('liGHT', 'heavy') == 'heavy'
+    assert match_casing('LIght', 'heavy') == 'heavy'
+    assert match_casing('LighT', 'heavy') == 'heavy'
+    
+    assert match_casing('RANCH', 'hyperbole') == 'HYPERBOLE'
+    assert match_casing('Ranch', 'hyperbole') == 'Hyperbole'
+    assert match_casing('ranch', 'hyperbole') == 'hyperbole'
+    assert match_casing('rANCH', 'hyperbole') == 'hyperbole'    
+    assert match_casing('raNCH', 'hyperbole') == 'hyperbole'
+    assert match_casing('RAnch', 'hyperbole') == 'hyperbole'
+    assert match_casing('RancH', 'hyperbole') == 'hyperbole'
+    
+    assert match_casing('HEIGHT', 'crouch') == 'CROUCH'
+    assert match_casing('Height', 'crouch') == 'Crouch'
+    assert match_casing('height', 'crouch') == 'crouch'
+    assert match_casing('hEIGHT', 'crouch') == 'crouch'    
+    assert match_casing('heiGHT', 'crouch') == 'crouch'
+    assert match_casing('HEIght', 'crouch') == 'crouch'
+    assert match_casing('HeighT', 'crouch') == 'crouch'
+    
+    assert match_casing('MIGHTY', 'henceforth') == 'HENCEFORTH'
+    assert match_casing('Mighty', 'henceforth') == 'Henceforth'
+    assert match_casing('mighty', 'henceforth') == 'henceforth'
+    assert match_casing('mIGHTY', 'henceforth') == 'henceforth'    
+    assert match_casing('migHTY', 'henceforth') == 'henceforth'
+    assert match_casing('MIGhty', 'henceforth') == 'henceforth'
+    assert match_casing('MightY', 'henceforth') == 'henceforth'
