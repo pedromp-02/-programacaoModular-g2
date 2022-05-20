499e2d54907ba8c22d1f79ba6021130d620602f5
Guilherme Gomes <77558872+guilhermerrg15@users.noreply.github.com>
2022-05-20 16:11:54 -0300

    Update programa1Main.py

--- programa1Main.py
+++ programa1Main.py
@@ -2,3 +2,12 @@
 from manipulacao import *
 from substituicao import *
 
+'''
+abrir arquivo
+usar as funcoes dos outros modulos
+devolver arquivo modificado
+'''
+f = open("teste.txt", "w")
+con = gera_string(f) # vai gerar uma var string com o conteudo o arquivo
+conteudo_final = substitui_palavra(con,"Palavra","PalavraSub")
+gera_arquivo(conteudo_final)
