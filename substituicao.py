9b31557f65e33484a9fe18988c0452f7c0a7c07b
Patrick-Haselof <82117539+Patrick-Haselof@users.noreply.github.com>
2022-05-24 09:57:10 -0300

    Update substituicao.py

--- substituicao.py
+++ substituicao.py
@@ -13,5 +13,7 @@ def substitui_palavra(txt,w,s):
             word += c
             if(compara_palavras(word,w)):       #Usa modulo auxiliar para comparacao
                 word = match_casing(word,s)     #Usa modulo auxiliar para substituicao
-            
     return t
+
+#[IMPORTANTE]: Por algum motivo o match_casing nao funciona para ajustar palavras em all caps,
+#funciona em aplicacao direta (print(match_casing("HELLO",s))) mas nao em variaveis (word = match_casing(word,s)), trata como uma palavra totalmente miniscula
