7d321781059c04a53ac923fbeb784491c52a25e6
Patrick-Haselof <82117539+Patrick-Haselof@users.noreply.github.com>
2022-05-24 09:52:34 -0300

    Update substituicao.py

--- substituicao.py
+++ substituicao.py
@@ -1,66 +1,17 @@
 # módulo de substituição de palavras em strings
 
-'''
-#Prototipo, erros de match_casing a resolver
-
-def replace_word_in_text(txt,w,s):
-    x = txt.split(" ")
+def substitui_palavra(txt,w,s):
     word = ""
-    aux_word1 = ""
-    aux_word2 = ""
-    aux_word3 = ""
     t = ""
-    counter = 0
-    aux_counter = 0
-    
-    for i in x:
-        word = ""
-        aux_word = ""
-        for c in i:
-            if(c == "."):       #Ajuda no detector de caso de "..."
-                aux_word1 += c
-                aux_counter += 1
-                word += c
-                
-            else:
-                word += c
-
-            if(compara_palavras(word,w)):       #Palavra match detectada
-                counter = 1
-                word = match_casing(word,s)
-
-            elif(aux_counter == 3):     #Caso de "..." confirmado
-                aux_word2 += c
-
-                if(compara_palavras(aux_word2[1:],w)):
-                    counter = 1
-                    for a in word:
-                        if(a == "."):
-                            break
-                        aux_word3 += a
-
-                    word = ""
-                    word += aux_word3 + aux_word1 + match_casing(aux_word2[1:],s)
-                    aux_counter = 0
-                    aux_word1 = ""
-                    aux_word2 = ""
-                    aux_word3 = ""
-     
-            if(c == " "):       #Break ao terminar a palavra
-                aux_word1 = ""
-                aux_counter = 0
-                break
+    for c in txt:                               #Pesquisa char por char
+        if(c == ' ' or c == '.'):               #Verificador para o timing de adicionamento para to texto resultado
+            t += word
+            t += c
+            word = ""
             
-        if(counter == 1):
-            t+= " "
-            t+=word
-            counter = 0
+        else:                                   #Constroi palavaras
+            word += c
+            if(compara_palavras(word,w)):       #Usa modulo auxiliar para comparacao
+                word = match_casing(word,s)     #Usa modulo auxiliar para substituicao
             
-        else:
-            t+= " "
-            t+=i
     return t
-'''
-
-def substitui_palavra(texto,palavra,sub):
-    return "Teste mock"
