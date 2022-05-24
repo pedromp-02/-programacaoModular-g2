2312dc1bcd88865ee3705dbd5f7f4cd9544432d7
Patrick-Haselof <82117539+Patrick-Haselof@users.noreply.github.com>
2022-05-24 09:59:09 -0300

    Update substituicao.py

--- substituicao.py
+++ substituicao.py
@@ -1,5 +1,21 @@
 # módulo de substituição de palavras em strings
 
+def compara_palavras(palavra1,palavra2):
+    palavra1lower = palavra1.casefold()
+    palavra2lower = palavra2.casefold()
+    return palavra1lower == palavra2lower
+
+def match_casing(palavra1,palavra2):
+    if palavra1.isupper(): 
+        return palavra2.upper() # upper
+    if palavra1.islower():
+        return palavra2.lower() # lower
+    else:
+        if palavra1.istitle():
+            return palavra2.title() # title
+        else:
+            return palavra2.lower() # default
+
 def substitui_palavra(txt,w,s):
     word = ""
     t = ""
