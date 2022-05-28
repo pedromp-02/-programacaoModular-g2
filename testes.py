2638057340582c2d008751c1b42d23aa538352c8
Igor <86137378+Isomo1@users.noreply.github.com>
2022-05-28 00:20:15 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -130,12 +130,13 @@ def test_match_casing():
     
 # Testes do módulo de substituição de palavras:
 def test_substitui_palavra():
-    assert substitui_palavra('a string', 'a', 'another') == 'another string'
-    assert substitui_palavra('example', 'EXAMPLE', 'TEST') == 'test'
-    assert substitui_palavra('HELLO world', 'hello', 'HI') == 'HI world'
-    assert substitui_palavra('Make it WORK', 'work', 'RUN') == 'Make it RUN'
-    assert substitui_palavra('Have a good DAY', 'dAy', 'EvEnInG') == 'Have a good EVENING'
+    assert substitui_palavra('small test', 'small', 'big') == 'big test'
+    assert substitui_palavra('EXAMPLE', 'EXAMPLE', 'TEST') == 'TEST'
+    assert substitui_palavra('A string', 'a', 'another') == 'Another string'
+    assert substitui_palavra('hello world', 'HELLO', 'hi') == 'hi world'
+    assert substitui_palavra('This is sPECIFIC', 'SpeCiFiC', 'nEAT') == 'This is neat'
+    assert substitui_palavra('The following word is deleted', 'DELETED', '') == 'The following word is '
+    assert substitui_palavra('There are 365 days in a year.', '365', '999') == 'There are 365 days in a year.'
+    assert substitui_palavra('Make it wORK', 'WoRk', 'run') == 'Make it run'
+    assert substitui_palavra('Have a good DaY', 'DAY', 'EVENING') == 'Have a good evening'
     assert substitui_palavra('This is clearly a test', 'cLEARLY', 'simply') == 'This is simply a test'
-    assert substitui_palavra('The following word is deleted', 'DeLEteD', '') == 'The following word is '
-    assert substitui_palavra('That is SpEcIfIc', 'SpecifiC', 'neat') == 'That is neat'
-    assert substitui_palavra('There are 365 days in a year', '365', '21') == 'There are 365 days in a year'
