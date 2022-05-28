e2484be4a729f8968acd45ce516fc9d7cf5a52f3
Igor <86137378+Isomo1@users.noreply.github.com>
2022-05-28 00:35:08 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -137,6 +137,6 @@ def test_substitui_palavra():
     assert substitui_palavra('This is sPECIFIC', 'SpeCiFiC', 'nEAT') == 'This is neat'
     assert substitui_palavra('The following word is deleted', 'DELETED', '') == 'The following word is '
     assert substitui_palavra('There are 365 days in a year.', '365', '999') == 'There are 365 days in a year.'
-    assert substitui_palavra('Make it wORK', 'WoRk', 'run') == 'Make it run'
+    assert substitui_palavra('Make it wORK', 'work', 'rUN') == 'Make it run'
     assert substitui_palavra('Have a good DaY', 'DAY', 'EVENING') == 'Have a good evening'
     assert substitui_palavra('This is clearly a test', 'cLEARLY', 'simply') == 'This is simply a test'
