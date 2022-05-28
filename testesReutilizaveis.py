fc2f0e381c51c4b6decde3bd025d7e4c3a695ac3
Igor <86137378+Isomo1@users.noreply.github.com>
2022-05-28 00:46:20 -0300

    Update and rename testes.py to testesReutilizaveis.py

--- testesReutilizaveis.py
+++ testesReutilizaveis.py
@@ -1,6 +1,5 @@
 from casingComp import *
 from manipulacao import *
-from substituicao import *
 
 # Testes do módulo de comparação e casing match de palavras:
 def test_compara_palavras():
@@ -128,15 +127,4 @@ def test_match_casing():
     assert match_casing('MIGhty', 'henceforth') == 'henceforth'
     assert match_casing('MightY', 'henceforth') == 'henceforth'
     
-# Testes do módulo de substituição de palavras:
-def test_substitui_palavra():
-    assert substitui_palavra('a string', 'a', 'another') == 'another string'
-    assert substitui_palavra('EXAMPLE', 'EXAMPLE', 'TEST') == 'TEST'
-    assert substitui_palavra('Small test', 'small', 'big') == 'Big test'
-    assert substitui_palavra('hello world', 'HELLO', 'hi') == 'hi world'
-    assert substitui_palavra('This is sPECIFIC', 'SpeCiFiC', 'nEAT') == 'This is neat'
-    assert substitui_palavra('The following word is deleted', 'DELETED', '') == 'The following word is '
-    assert substitui_palavra('There are 365 days in a year.', '365', '999') == 'There are 365 days in a year.'
-    assert substitui_palavra('Make it wORK', 'work', 'rUN') == 'Make it run'
-    assert substitui_palavra('Have a good DaY', 'DAY', 'EVENING') == 'Have a good evening'
-    assert substitui_palavra('This is clearly a test', 'cLEARLY', 'simply') == 'This is simply a test'
+# Testes do módulo de manipulação de strings e arquivos:
