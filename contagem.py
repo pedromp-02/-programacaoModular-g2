407e3f38ecdeee8b28b8e12e9f41576c96afaf60
lvr44 <105324948+lvr44@users.noreply.github.com>
2022-06-03 17:30:49 -0300

    Update contagem.py

--- contagem.py
+++ contagem.py
@@ -1,4 +1,25 @@
-from casingComp import *
-
 def gera_matriz_indice(texto):
-    return 'texto matriz'
+    i = 0
+    palavras = texto.split()
+    matriz = []
+    usadas = []
+
+    while(i < len(palavras)):
+      if(palavras[i] not in usadas):
+
+        palavras[i] = match_casing("Title", palavras[i]) # deixar "Title" ou "lower"???
+        
+        sub_lista = [palavras[i], 1]
+        usadas.append(palavras[i])
+        matriz.append(sub_lista)
+        
+      else:
+        j = 0
+        while(usadas[j] != palavras[i]):
+          j +=1
+        
+        matriz[j][1] += 1
+
+      i+=1
+    
+    return matriz # 'texto matriz'
