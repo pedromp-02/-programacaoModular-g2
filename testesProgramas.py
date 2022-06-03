d4e055fcc1646b538f1a52ff8be0adf30035822d
Igor <86137378+Isomo1@users.noreply.github.com>
2022-06-03 19:49:13 -0300

    Create testesProgramas.py

---
+++ testesProgramas.py
@@ -0,0 +1,10 @@
+# Testes do Programa 1:
+def test_prog1():
+    nome_arq = "testando.txt"
+    with open(nome_arq, "w") as f:
+        f.write("The usage of certain applications may influence other applications in a positive manner.\n")
+    f.close()
+    with open(nome_arq, "r") as f:
+        texto = gera_string(nome_arq)
+    f.close()
+    assert substitui_palavra(texto, "applications", "apps") == "The usage of certain apps may influence other apps in a positive manner.\n"
