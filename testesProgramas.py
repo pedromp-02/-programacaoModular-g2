c5bd409dd08dab16f023d8bef3bedcafcf23e485
Igor <86137378+Isomo1@users.noreply.github.com>
2022-06-04 15:24:42 -0300

    teste programa2

--- testesProgramas.py
+++ testesProgramas.py
@@ -1,9 +1,11 @@
 from reutilizaveis.manipulacao import *
 from prog1.substituicao import *
+from prog2.contagem import *
+from prog3.lista_linhas import *
 
 # Testes do Programa 1:
 def test_prog1():
-    nome_arq = "testando.txt"
+    nome_arq = "testando1.txt"
     with open(nome_arq, "w") as f:
         f.write("The usage of certain applications may influence other applications in a positive manner.\n")
     f.close()
@@ -11,3 +13,16 @@ def test_prog1():
         texto = gera_string(nome_arq)
     f.close()
     assert substitui_palavra(texto, "applications", "apps") == "The usage of certain apps may influence other apps in a positive manner.\n"
+
+# Testes do Programa 2:
+def test_prog2():
+    nome_arq = "testando2.txt" 
+    with open(nome_arq, "w") as f:
+        f.write("This is a test:\nA SIMPLE TEST.\nthis This THIS tHIS!\n234, 487, 222\né#ààà@ú")
+    f.close()
+    with open(nome_arq, "r") as f:
+        texto = gera_string(nome_arq)
+    f.close()
+    esperado = 'This - 5\nIs - 1\nA - 2\nTest - 2\nSimple - 1\nÉ - 1\nÀ - 3\nÚ - 1\n'
+    assert gera_matriz_indice(texto) == esperado
+    
